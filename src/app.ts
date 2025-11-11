c7558e19ff1b413f44561fc2c9cd6167f2df92bb
alexsm <alexsm@taltech.ee>
2025-11-12 01:22:30 +0200

    change cors settings

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,21 @@ import router from "./routes/router";
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://asmirnov.pages.dev'
+];
+
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
